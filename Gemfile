752b63a9c8f602192cc713869a51a51c6ba6454a
David Mandeeli (Shiro) <davidmandeeli@gmail.com>
2013-03-15 19:26:23 +0800

    First Commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem "middleman", "~> 3.0.11"
+
+# Compass plugins
+gem 'susy', "~> 1.0.5" # Susy
+gem 'compass-h5bp', "~> 0.1.0" # HTML5 Boilerplate styles
---
+++ source/javascripts/script.js
@@ -0,0 +1,46 @@
+/* Author: */
+
+$('#ptg').cycle({
+	delay: -1000,
+	random: 1
+});
+
+$('#sh').cycle({ 
+    fx:    'scrollDown', 
+    delay: -3000 
+});
+
+$('#st').cycle({
+	delay: -3000,
+	random: 1
+});
+
+$('#fs').cycle({ 
+    fx:    'scrollLeft', 
+    delay: -1000 
+});
+
+$('#trip').cycle({
+	delay: -500,
+	random: 1
+});
+
+$('#cs').cycle({ 
+    fx:    'scrollUp', 
+    delay: -4000 
+});
+
+$('#mt').cycle({
+	delay: -4000,
+	random: 1
+});
+
+$('#gh').cycle({ 
+    fx:    'scrollDown', 
+    delay: -2000 
+});
+
+$('#gal').cycle({
+	delay: -3000,
+	random: 1
+});
